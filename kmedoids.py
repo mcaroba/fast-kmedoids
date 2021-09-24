51e10a9afd01905d97e51941fd1dcb83a0d280a4
Miguel Caro <mcaroba@gmail.com>
2021-09-24 21:52:58 +0300

    Added initialization and iterative improvement based on coherence criterion

--- kmedoids.py
+++ kmedoids.py
@@ -2,7 +2,7 @@ import numpy as np
 import random
 from src.fast_module import kmedoids_module as kmod
 
-def kMedoids(D, k, tmax=100, init_Ms="random", n_iso=None):
+def kMedoids(D, k, tmax=100, init_Ms="random", n_iso=None, n_inits=1, incoherence="rel", verbosity=1):
     # determine dimensions of distance matrix D
     m, n = D.shape
     # Assert square distance matrix
@@ -15,61 +15,48 @@ def kMedoids(D, k, tmax=100, init_Ms="random", n_iso=None):
         M = np.arange(n)
         np.random.shuffle(M)
         M = np.sort(M[:k])
+        n_iso = 0
     elif str(init_Ms) == "isolated":
         if n_iso is None:
             n_iso = k
+            n_inits = 1
         elif n_iso > k:
             n_iso = k
+            n_inits = 1
         else:
             assert type(n_iso) == int
             if n_iso == 0:
                 raise Exception('You must choose a non-zero number of \
                                  isolated medoids or switch to random init.')
-        summed_distances = np.sum(D, axis = 0)
-        av_distance = np.sum(D) / float(n)**2
-        M = np.empty(0, dtype=int)
-        # Find most isolated sample to use as first medoid
-        i = np.argmax(summed_distances)
-        M = np.append(M, i)
-        # Find new medoids which are furthest away from previous medoids
-        for i in range(1, n_iso):
-            summed_distances = np.zeros(n)
-            for j in range(0, len(M)):
-                summed_distances += D[:, M[j]]
-            # avoid repeating medoids
-            summed_distances[M] = 0 
-            j = np.argmax(summed_distances)
-            M = np.append(M, j)
-        # Randomize the rest
-        for i in range(n_iso, k):
-            rand = np.random.random_integers(0,n-1)
-            # Make sure that the random medoid is not already chosen and
-            # is not too close to another medoid by imposing the inter-
-            # medoid distance to be larger than the average inter-sample
-            # distance
-            tries = 0
-            while rand in M or any(D[rand,j] < av_distance for j in M):
-                tries += 1
-                rand = np.random.random_integers(0,n-1)
-                if tries == 100:
-                    tries = 0
-                    av_distance *= 0.9
-            M = np.append(M, rand)
+        if n_inits == 1: # else flag for fast Fortran execution
+            M = kmod.find_isolated(D, k, n_iso)
     elif len(init_Ms) == k:
         M = init_Ms
+        n_inits = 1
     elif len(init_Ms) < k:
         M = init_Ms
-        for i in range(len(init_Ms), k):
-            rand = np.random.random_integers(0,n-1)
-            while rand in M:
+        if n_inits == 1: # else flag for fast Fortran execution
+            for i in range(len(init_Ms), k):
                 rand = np.random.random_integers(0,n-1)
-            M = np.append(M, rand)
+                while rand in M:
+                    rand = np.random.random_integers(0,n-1)
+                M = np.append(M, rand)
     else:
         raise Exception("intial medoids vector has the wrong length")
 
 #   Use Fortran module to run kmedoids algorithm fast (number of clusters is inferred
 #   from size of M)
-    M, C = kmod.fast_kmedoids(D, tmax, M)
+    if n_inits == 1:
+        M, C = kmod.fast_kmedoids(D, tmax, M)
+    else:
+        if str(init_Ms) == "random" or str(init_Ms) == "isolated":
+            M_init_mode = np.array(str(init_Ms), dtype='c')
+            M, C = kmod.fast_kmedoids_iterative(D, k, tmax, n_inits, n_iso, incoherence, [0], M_init_mode, verbosity)
+        else:
+            n_iso = 0
+            M_init_mode = np.array("random+", dtype='c')
+            M, C = kmod.fast_kmedoids_iterative(D, k, tmax, n_inits, n_iso, incoherence, init_Ms, M_init_mode, verbosity)
+
 #   Return results like CB's code for compatibility
     C_dict = {}
     for i in range(0, k):
--- src/kmedoids.f90
+++ src/kmedoids.f90
@@ -5,6 +5,10 @@ module kmedoids_module
 
 contains
 
+
+
+
+
 subroutine fast_kmedoids(D, tmax, M_init, M, C)
 
   implicit none
@@ -24,11 +28,11 @@ subroutine fast_kmedoids(D, tmax, M_init, M, C)
   if( size(D,1) /= size(D,2) )then
     write(*,*) "ERROR: distance matrix D must be square!"
     return
-  else if( size(M) > size(D,1) )then
+  else if( size(M_init) > size(D,1) )then
     write(*,*) "ERROR: number of medoids can't be larger than number of data points!"
     return
-  else if( any(M < 0) .or. any(M >= size(D,1)) )then
-    write(*,*) "ERROR: bad medoids initialization (elements in M out of range)!"
+  else if( any(M_init < 0) .or. any(M_init >= size(D,1)) )then
+    write(*,*) "ERROR: bad medoids initialization (elements in M_init out of range)!"
     return
   else if( tmax < 0 )then
     write(*,*) "WARNING: tmax < 0, nothing to do!"
@@ -109,6 +113,282 @@ subroutine fast_kmedoids(D, tmax, M_init, M, C)
 
   M = M - 1
   C = C - 1
+
+end subroutine
+
+
+
+
+
+
+subroutine find_isolated(D, nclusters, niso, M)
+
+  implicit none
+
+! Input variables
+  real*8, intent(in) :: D(:,:)
+  integer, intent(in) :: nclusters, niso
+! Output variables
+  integer, intent(out) :: M(1:nclusters)
+! Internal variables
+  real*8, allocatable :: summed_D(:)
+  real*8 :: av_D, rand
+  integer :: npts, i, j, k, l, tries
+  logical :: is_j_in_M, is_j_too_close
+
+  if( niso < 1 )then
+    write(*, *) "WARNING: niso < 1, so nothing to do!"
+    return
+  end if
+
+  npts = size(D,2)
+  allocate( summed_D(1:npts) )
+
+  av_D = sum(D) / float(npts)**2
+
+! Find the first one
+  do i = 1, npts
+    summed_D(i) = sum( D(1:npts, i) )
+  end do
+  i = maxloc(summed_D, 1)
+  M(1) = i
+
+! Find the rest
+  do i = 2, niso
+    summed_D = 0.d0
+    do j = 1, i-1
+      k = M(j)
+      do l = 1, npts
+        if( l /= k )then
+          summed_D(l) = summed_D(l) + D(l, k)
+        end if
+      end do
+    end do
+    j = maxloc(summed_D, 1)
+    M(i) = j
+  end do
+
+! Randomize the rest
+  do i = niso+1, nclusters
+    tries = 0
+    do
+      call random_number(rand)
+      j = floor(rand * float(npts)) + 1
+!     Check if j is already in M
+      is_j_in_M = .false.
+      do k = 1, i-1
+        if( M(k) == j )then
+          is_j_in_M = .true.
+          exit
+        end if
+      end do
+!     Check if j is too close
+      if( .not. is_j_in_M )then
+        is_j_too_close = .false.
+        do k = 1, i-1
+          if( D(j,M(k)) < av_D )then
+            is_j_too_close = .true.
+            tries = tries + 1
+            exit
+          end if
+        end do      
+      end if
+!     Relax search criteria
+      if( tries == 100 )then
+        tries = 0
+        av_D = 0.9d0 * av_D
+      end if
+!     In case of success, we break the loop
+      if( .not. is_j_in_M .and. .not. is_j_too_close )then
+        M(i) = j
+        exit
+      end if
+    end do
+  end do
+
+  M = M - 1
+
+  deallocate( summed_D )
+
+end subroutine
+
+
+
+
+
+
+subroutine find_random(npts, nclusters, M_input, is_empty, M)
+
+  implicit none
+
+! Input variables
+  integer, intent(in) :: npts, nclusters
+  integer, intent(in) :: M_input(:)
+  logical, intent(in) :: is_empty
+! Output variables
+  integer, intent(out) :: M(1:nclusters)
+! Internal variables
+  real*8 :: rand
+  integer :: nstart, i, j, k
+  logical :: is_j_in_M
+
+  if( .not. is_empty )then
+    nstart = size(M_input)
+    if( size(M_input) > nclusters )then
+      write(*,*) "ERROR: size(M_input) can't be larger than nclusters"
+      return
+    end if
+  else
+    nstart = 0
+  end if
+
+! Fix Python convention; we fix it back at the end
+  if( nstart > 0 )then
+    M(1:nstart) = M_input(1:nstart) + 1
+  end if
+
+! Randomize the rest
+  do i = nstart+1, nclusters
+    do
+      call random_number(rand)
+      j = floor(rand * float(npts)) + 1
+!     Check if j is already in M
+      is_j_in_M = .false.
+      do k = 1, i-1
+        if( M(k) == j )then
+          is_j_in_M = .true.
+          exit
+        end if
+      end do
+!     In case of success, we break the loop
+      if( .not. is_j_in_M )then
+        M(i) = j
+        exit
+      end if
+    end do
+  end do
+
+  M = M - 1
+
+
+end subroutine
+
+
+
+
+
+subroutine fast_kmedoids_iterative(D, nclusters, tmax, n_inits, n_iso, incoherence, M_in, M_init_mode, verbosity, M, C)
+
+  implicit none
+
+! Input variables
+  real*8, intent(in) :: D(:, :)
+  integer, intent(in) :: tmax, n_inits, n_iso, nclusters
+  integer, intent(in) :: M_in(:), verbosity
+  character(len=*), intent(in) :: incoherence
+  character(len=*), intent(in) :: M_init_mode
+! Output variables
+  integer, intent(out) :: C(1:size(D,1)), M(1:nclusters)
+! Internal variables
+  real*8 :: R, R_prev
+  integer, allocatable :: M_init(:)
+  integer :: C_i(1:size(D,1)), M_i(1:nclusters), M_ii(1:nclusters)
+  integer :: nstart, npts, i
+  logical :: is_empty
+
+  npts = size(D,1)
+
+  if( M_init_mode == "random" )then
+    is_empty = .true.
+  else if( M_init_mode == "random+" )then
+    nstart = size(M_in)
+    allocate( M_init(1:nstart) )
+    M_init(1:nstart) = M_in(1:nstart)
+    is_empty = .false.
+  end if
+
+  if( M_init_mode(1:6) /= "random" .and. M_init_mode(1:8) /= "isolated" )then
+    write(*,*) "ERROR: I don't understand M_init_mode, it must be random|isolated"
+    return
+  end if
+
+  R_prev = 1.d100
+  if( verbosity >= 1 )then
+    write(*,"(A,A,A)") "# Iteration; Residual ('", trim(adjustl(incoherence)), "' incoherence criterion)"
+  end if
+  do i = 1, n_inits
+    if( M_init_mode(1:6) == "random" )then
+!     Set up initial M
+      call find_random(npts, nclusters, M_init, is_empty, M_ii)
+    else if( M_init_mode == "isolated" )then
+!     Set up initial M
+      call find_isolated(D, nclusters, n_iso, M_ii)
+    end if
+    call fast_kmedoids(D, tmax, M_ii, M_i, C_i)
+    call get_incoherence(D, M_i, C_i, incoherence, R)
+    if( R < R_prev )then
+      M = M_i
+      C = C_i
+      R_prev = R
+      if( verbosity >= 1 )then
+        write(*,*) i, R
+      end if
+    end if
+  end do
+
+  if( allocated(M_init) )deallocate( M_init )
+
+end subroutine
+
+
+
+
+
+
+subroutine get_incoherence(D, M, C, incoherence, R)
+
+  implicit none
+
+! Input variables
+  real*8, intent(in) :: D(:,:)
+  integer, intent(in) :: M(:), C(:)
+  character(len=*) :: incoherence
+! Output variables
+  real*8, intent(out) :: R
+! Internal variables
+  integer, allocatable :: cluster_size(:)
+  integer :: npts, nclusters, i, j, k, k2
+
+  npts = size(D, 1)
+  nclusters = size(M)
+
+  if( incoherence == "rel" )then
+    allocate( cluster_size(1:nclusters) )
+
+    cluster_size = 0
+    do i = 1, npts
+      cluster_size(C(i)+1) = cluster_size(C(i)+1) + 1
+    end do
+
+    R = 0.d0
+    do i = 1, nclusters
+      k = M(i)+1
+      do j = 1, npts
+        k2 = C(j) + 1
+        R = R + D(k2, k)/float(cluster_size(i)) 
+      end do
+    end do
+    deallocate( cluster_size )
+  else if( incoherence == "tot" )then
+    R = 0.d0
+    do i = 1, nclusters
+      k = M(i)+1
+      do j = 1, npts
+        k2 = C(j) + 1
+        R = R + D(k2, k) 
+      end do
+    end do
+  end if
 
 end subroutine
 
